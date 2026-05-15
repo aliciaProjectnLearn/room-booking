2f83039c1157763a0fa20f650cfcfad97b5d43c8
boyajvv <boyarizky786@gmail.com>
2026-05-15 18:25:45 +0700

    embuat structure database dan seeder awal

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Admin
+        User::create([
+            'name' => 'Admin Keisha',
+            'email' => 'admin1@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin',
+            'phone_number' => '081234567890',
+        ]);
+
+        User::create([
+            'name' => 'Admin Cia',
+            'email' => 'admin2@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin',
+            'phone_number' => '081234567890',
+        ]);
+
+        // Guru 1
+        User::create([
+            'name' => 'Adang Nugroho',
+            'email' => 'adang@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'guru',
+            'phone_number' => '081298765432',
+        ]);
+
+        // Guru 2
+        User::create([
+            'name' => 'Boya Rizky Agung',
+            'email' => 'boya@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'guru',
+            'phone_number' => '081211223344',
+        ]);
+    }
+}
